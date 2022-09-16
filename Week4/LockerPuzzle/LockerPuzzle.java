621a4df3ad751691960e808c529275d57b699a19
scottwegley <scottwegley@gmail.com>
2022-09-16 16:14:10 -0700

    finished locker puzzle

--- Week4/LockerPuzzle/LockerPuzzle.java
+++ Week4/LockerPuzzle/LockerPuzzle.java
@@ -2,19 +2,31 @@ package Week4.LockerPuzzle;
 
 public class LockerPuzzle {
     public static void main(String[] args) {
-        lockerSim(5);
+        lockerSim(8);
     }
-
-    public static void lockerSim(int n){
-        if(n<1) return;
+    //Weirdly finds the sqrt of n to the nearest full number.
+    public static void lockerSim(int n) {
+        if (n < 1)
+            return;
         System.out.println("Simulating " + n + " lockers");
         boolean[] lockers = new boolean[n];
         for (int i = 0; i < n; i++) {
             lockers[i] = false;
         }
 
-        for (boolean b : lockers) {
-            System.out.println(b);
+        for (int j = 1; j <= n; j++) {
+            for (int i = j - 1; i < n; i += j) {
+                lockers[i] = !lockers[i];
+            }
         }
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            if (lockers[i]) {
+                System.out.println("Locker " + i + " is open");
+                total++;
+            }
+        }
+        System.out.println(total + " lockers are open");
     }
 }
