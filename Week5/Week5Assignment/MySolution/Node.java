20014991da82703fb4c3aed1005a85b1a04292e7
scottwegley <scottwegley@gmail.com>
2022-09-30 16:15:38 -0700

    updated node to allow for priority

--- Week5/Week5Assignment/MySolution/Node.java
+++ Week5/Week5Assignment/MySolution/Node.java
@@ -4,13 +4,25 @@ public class Node<E> {
 
     E element;
     Node<E> next;
+    int priority;
 
     public Node(E element) {
         this.element = element;
+        this.next = null;
+    }
+
+    public Node() {
+        this.element = null;
+        this.next = null;
     }
 
     public void tag(Node<E> inNext){
         this.next = inNext;
+    }
+
+    @Override
+    public String toString() {
+        return element + " " + Integer.toString(priority);
     }
     
 }
