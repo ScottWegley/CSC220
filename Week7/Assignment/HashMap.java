d30983f64a598a923ff844fea0a5f0e3553c3fed
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-10 16:27:59 -0700

    created template hashmap file from base

---
+++ Week7/Assignment/HashMap.java
@@ -0,0 +1,23 @@
+package Week7.Assignment;
+
+public class HashMap<KEYTYPE,DATATYPE> extends HashMapBase<KEYTYPE,DATATYPE>{
+
+    @Override
+    protected int hash(Object key) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void add(Object key, Object e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int[] getSizes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
