2c52e5198ff01a1f754e111b86bcaaf2587fdd4b
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-24 16:39:12 -0700

    perfect & prime number classes with only stubs

---
+++ Week9/Assignment/PerfectNumber.java
@@ -0,0 +1,23 @@
+package Week9.Assignment;
+
+public class PerfectNumber implements PerfectNumberInterface {
+
+    @Override
+    public boolean isPerfect(long n) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public long getPerfect(long p) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long getNextPerfect(long p) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
---
+++ Week9/Assignment/PrimeNumber.java
@@ -0,0 +1,23 @@
+package Week9.Assignment;
+
+public class PrimeNumber implements PrimeNumberInterface {
+
+    @Override
+    public long[] primeFactors(long p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isPrime(long p) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public long nextPrime(long p) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
