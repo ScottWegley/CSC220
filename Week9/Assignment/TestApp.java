3fe7b95674493dea741622bdcec2bf6464fa70a7
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-24 16:40:21 -0700

    added code from assignment instructions to testing file

--- Week9/Assignment/TestApp.java
+++ Week9/Assignment/TestApp.java
@@ -1,5 +1,25 @@
 package Week9.Assignment;
 
 public class TestApp {
-    
+    public static void main(String[] args) {
+        PerfectNumberInterface pf = new PerfectNumber();
+        for (int p = 1; p < 20; ++p) {
+            long perfect = pf.getPerfect(p);
+            System.out.println("From main: " + p + " " + perfect + " " + pf.isPerfect(perfect));
+        }
+
+        PrimeNumberInterface pr = new PrimeNumber();
+        for (int i = 1; i < 20; ++i) {
+            if (pr.isPrime(i)) {
+                System.out.println(i + " is Prime");
+            } else {
+                long factors[] = pr.primeFactors(i);
+                System.out.print(i + " = ");
+                for (int f = 0; f < factors.length - 1; ++f) {
+                    System.out.print(factors[f] + " * ");
+                }
+                System.out.println(factors[factors.length - 1]);
+            }
+        }
+    }
 }
