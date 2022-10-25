922af8f4308598d5fa86e3822d5dc7efa85e7fd0
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-24 17:57:21 -0700

    commented out assignment code and added debugs

--- Week9/Assignment/TestApp.java
+++ Week9/Assignment/TestApp.java
@@ -2,7 +2,11 @@ package Week9.Assignment;
 
 public class TestApp {
     public static void main(String[] args) {
-        PerfectNumberInterface pf = new PerfectNumber();
+
+        PerfectNumber myPf = new PerfectNumber();
+        System.out.println(myPf.getNextPerfect(2L));
+
+        /* PerfectNumberInterface pf = new PerfectNumber();
         for (int p = 1; p < 20; ++p) {
             long perfect = pf.getPerfect(p);
             System.out.println("From main: " + p + " " + perfect + " " + pf.isPerfect(perfect));
@@ -20,6 +24,6 @@ public class TestApp {
                 }
                 System.out.println(factors[factors.length - 1]);
             }
-        }
+        } */
     }
 }
