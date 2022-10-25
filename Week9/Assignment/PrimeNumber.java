a9e91046bc91b2cc6dac3d80baf789a1fb64727e
Scott Wegley <scbrwegley@protonmail.ch>
2022-10-24 18:39:40 -0700

    start at 2, divide by found prime factors until all are checked

--- Week9/Assignment/PrimeNumber.java
+++ Week9/Assignment/PrimeNumber.java
@@ -7,6 +7,14 @@ public class PrimeNumber implements PrimeNumberInterface {
     @Override
     public long[] primeFactors(long p) {
         ArrayList<Long> factors = new ArrayList<Long>();
+        for (long i = 2L; i <= (p); i++) {
+            if (p % i == 0) {
+                if (isPrime(i)) {
+                    factors.add(i);
+                    p = p / i;
+                }
+            }
+        }
     }
 
     @Override
