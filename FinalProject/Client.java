cdc3659e8c0ace6b8a0da203fde5c78d7aa9fe60
scottwegley <scottwegley@gmail.com>
2022-11-28 23:20:42 -0800

    client layout setup

--- FinalProject/Client.java
+++ FinalProject/Client.java
@@ -10,5 +10,37 @@ import java.net.Socket;
 import javax.swing.*;
 
 public class Client {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Rock-Paper-Scissors Client");
+        JPanel panel = new JPanel();
+        JMenuBar mb = new JMenuBar();
+        mb.add(new JMenu("Disconnected"));
+        mb.add(new JMenu("PLAYER ?"));
+        mb.add(new JMenu("Score: 0"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
 
+        JLabel lblSSID = new JLabel("Input Session ID:");
+        JTextField inputSSID = new JTextField(6);
+        JButton btnConnect = new JButton("Connect");
+        panel.add(lblSSID);
+        panel.add(inputSSID);
+        panel.add(btnConnect);
+
+        JButton btnRock = new JButton("Rock");
+        JButton btnScissors = new JButton("Scissors");
+        JButton btnPaper = new JButton("Paper");
+
+        btnRock.setEnabled(false);
+        btnScissors.setEnabled(false);
+        btnPaper.setEnabled(false);
+
+        frame.getContentPane().add(BorderLayout.LINE_START, btnRock);
+        frame.getContentPane().add(BorderLayout.CENTER, btnPaper);
+        frame.getContentPane().add(BorderLayout.LINE_END, btnScissors);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.setVisible(true);
+
+    }
 }
