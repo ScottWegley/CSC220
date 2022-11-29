79a91eb3bb05d612941e8b34ea97a6dbf7cd9a53
scottwegley <scottwegley@gmail.com>
2022-11-28 23:09:06 -0800

    function for Rock Beats Scissors etc. logic

--- FinalProject/RPS_MOVE.java
+++ FinalProject/RPS_MOVE.java
@@ -5,4 +5,17 @@ public enum RPS_MOVE {
     PAPER,
     SCISSORS;
 
+    public RPS_OUTCOME fight(RPS_MOVE enemyMove) {
+        if (this == enemyMove) {
+            return RPS_OUTCOME.DRAW;
+        }
+        switch (this) {
+            case ROCK:
+                return ( enemyMove == PAPER ? RPS_OUTCOME.DEFEAT : RPS_OUTCOME.VICTORY);
+            case PAPER:
+                return ( enemyMove == SCISSORS ? RPS_OUTCOME.DEFEAT : RPS_OUTCOME.VICTORY);
+            default: //Scissors Case
+                return ( enemyMove == ROCK ? RPS_OUTCOME.DEFEAT : RPS_OUTCOME.VICTORY);
+        }
+    }
 }
